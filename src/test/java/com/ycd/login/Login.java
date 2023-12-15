d44eb51357b621903d06d83c02189496895af6aa
Ayyanayagi C <ayyanayagi@tendersoftware.in>
2023-12-15 15:16:54 +0530

    initial

---
+++ src/test/java/com/ycd/login/Login.java
@@ -0,0 +1,24 @@
+package com.ycd.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+	@Test
+	public void login()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://dev.yourchildsday.com/authentication/");
+		WebElement email=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/form/div/div[2]/div/input"));
+		email.sendKeys("peter@yopmail.com");
+		WebElement psw=driver.findElement(By.xpath("//input[@type='password']"));
+		psw.sendKeys("Vss@1234");
+		WebElement lgnbtn=driver.findElement(By.xpath("//button[@type='submit']"));
+		lgnbtn.click();
+		}
+
+}
